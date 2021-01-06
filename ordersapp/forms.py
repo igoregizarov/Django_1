c8fa84a5883d46423943c9fdaf981e5f7b9635e5
Igor Egizarov <i_gor88@mail.ru>
2021-01-07 01:43:40 +0700

    второй курс Django ДЗ№3 задание со звездочкой-заказы в админке

--- ordersapp/forms.py
+++ ordersapp/forms.py
@@ -14,6 +14,18 @@ class OrderForm(forms.ModelForm):
            field.widget.attrs['class'] = 'form-control'
 
 
+class OrderUpdateForm(forms.ModelForm):
+   class Meta:
+       model = Order
+       # exclude = ('user',)
+       fields = ['status']
+
+   def __init__(self, *args, **kwargs):
+       super(OrderUpdateForm, self).__init__(*args, **kwargs)
+       for field_name, field in self.fields.items():
+           field.widget.attrs['class'] = 'form-control'
+
+
 class OrderItemForm(forms.ModelForm):
    class Meta:
        model = OrderItem
